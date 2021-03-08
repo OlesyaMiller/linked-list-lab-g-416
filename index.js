a8e1d43bbe634abed7dff74b30ef0fe6cedda3c1
Olesya Miller <olesyam.miller@gmail.com>
2021-03-08 16:39:04 -0400

    clean up

---
+++ index.js
@@ -0,0 +1,21 @@
+function getName(node) {
+  return node.name 
+}
+
+function headNode(linkedList, collection){
+    return collection[linkedList]
+}
+
+function next(node, collection){
+    let nextAddress = node.next
+    return collection[`${nextAddress}`]
+}
+
+function nodeAt(index, linkedList, collection){
+    let currentNode = headNode(linkedList, collection);
+    for(let i = 0; i < index; i++){
+       currentNode = next(currentNode, collection);
+    }
+  
+    return currentNode;
+  }
